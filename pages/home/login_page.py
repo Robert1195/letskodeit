b55b51d6d2fde5109994006b90eb14f9b837c172
rew <szewczyk199511@gmail.com>
2023-03-20 14:57:12 +0100

    Refactor Your Page Object Class - Part 1

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -6,15 +6,38 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def login(self, username, password):
-        login_link = self.driver.find_element(By.LINK_TEXT, "Sign Up or Log In")
-        login_link.click()
+    # Locators:
+    _login_link = "Sign Up or Log In"
+    _email_field = "email"
+    _password_field = "login-password"
+    _login_btn = "login"
 
-        email_field = self.driver.find_element(By.ID, "email")
-        email_field.send_keys(username)
+    def get_login_link(self):
+        return self.driver.find_element(By.LINK_TEXT, self._login_link)
 
-        password_field = self.driver.find_element(By.ID, "login-password")
-        password_field.send_keys("Robert")
+    def get_email_field(self):
+        return self.driver.find_element(By.ID, self._email_field)
 
-        login_btn = self.driver.find_element(By.ID, "login")
-        login_btn.click()
+    def get_password_field(self):
+        return self.driver.find_element(By.ID, self._password_field)
+
+    def get_login_btn(self):
+        return self.driver.find_element(By.ID, self._login_btn)
+
+    def click_login_link(self):
+        self.get_login_link().click()
+
+    def enter_email(self, email):
+        self.get_email_field().send_keys(email)
+
+    def enter_password(self, password):
+        self.get_password_field().send_keys(password)
+
+    def click_login_btn(self):
+        self.get_login_btn().click()
+
+    def login(self, email, password):
+        self.click_login_link()
+        self.enter_email(email)
+        self.enter_password(password)
+        self.click_login_btn()
